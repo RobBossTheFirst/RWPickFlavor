e8151e16f926366b4f89e530c3c91fc70880c15c
Robin Kolze <johngaz@hotmail.de>
2017-12-09 13:01:10 +0100

    adds class

---
+++ RWPickFlavor/TestObject.swift
@@ -0,0 +1,18 @@
+//
+//  TestObject.swift
+//  RWPickFlavor
+//
+//  Created by Robin Kolze on 09.12.17.
+//  Copyright © 2017 Doublecheck IT. All rights reserved.
+//
+
+import Foundation
+
+class TestObject {
+    
+    public var value:Int = 3
+    
+    init(){
+        value = 5
+    }
+}
